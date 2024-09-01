45f17da8d1139a928fdf3e9831d29bf24c68605b
int-128 <1766420336@qq.com>
2024-09-01 21:36:13 +0800

    Update rotation

--- api/backend/roration.go
+++ api/backend/roration.go
@@ -6,9 +6,9 @@ import (
 
 type RotationReq struct {
 	g.Meta `path:"/backend/rotation/add" tags:"Rotation" method:"post" summary:"You first rotation api"`
-	PicUrl string `json:"pic_url"    v:"required#图片链接不能为空" dc:"图片链接"`
-	Link   string `json:"link"    v:"required#跳转链接不能为空" dc:"跳转链接"`
-	Sort   int    `json:"sort"         dc:"排序"`
+	PicUrl string `json:"pic_url"        v:"required#图片链接不能为空" dc:"图片链接"`
+	Link   string `json:"link"           v:"required#跳转链接不能为空" dc:"跳转链接"`
+	Sort   int    `json:"sort"           dc:"排序"`
 }
 type RotationRes struct {
 	RotationId int `json:"rotationId"`
@@ -19,3 +19,12 @@ type RotationDeleteReq struct {
 	Id     uint `v:"min:1#请选择需要删除的轮播图" dc:"轮播图id"`
 }
 type RotationDeleteRes struct{}
+
+type RotationUpdateReq struct {
+	g.Meta `path:"/backend/rotation/update/{Id}" method:"post" tags:"轮播图" summary:"修改轮播图接口"`
+	Id     uint   `json:"id"         v:"min:1#请选择需要修改的内容" dc:"轮播图Id"`
+	PicUrl string `json:"pic_url"    v:"required#图片链接不能为空" dc:"图片链接"`
+	Link   string `json:"link"       v:"required#跳转链接不能为空" dc:"跳转链接"`
+	Sort   int    `json:"sort"       dc:"排序"`
+}
+type RotationUpdateRes struct{}
