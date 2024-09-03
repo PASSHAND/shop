d923fff2e01fa997aa5cda6193f77538c510fb02
int-128 <1766420336@qq.com>
2024-09-03 18:54:11 +0800

    Role all

--- internal/controller/role.go
+++ internal/controller/role.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"context"
+	"github.com/gogf/gf/v2/util/gutil"
 	"goframe-shop/api/backend"
 	"goframe-shop/internal/model"
 	"goframe-shop/internal/service"
@@ -13,6 +14,7 @@ var Role = cRole{}
 type cRole struct{}
 
 func (a *cRole) Create(ctx context.Context, req *backend.RoleReq) (res *backend.RoleRes, err error) {
+	gutil.Dump(req.Name)
 	out, err := service.Role().Create(ctx, model.RoleCreateInput{
 		RoleCreateUpdateBase: model.RoleCreateUpdateBase{
 			Name: req.Name,
@@ -23,4 +25,36 @@ func (a *cRole) Create(ctx context.Context, req *backend.RoleReq) (res *backend.
 		return nil, err
 	}
 	return &backend.RoleRes{RoleId: out.RoleId}, nil
+}
+
+func (a *cRole) Delete(ctx context.Context, req *backend.RoleDeleteReq) (res *backend.RoleDeleteRes, err error) {
+	err = service.Role().Delete(ctx, req.Id)
+	return
+}
+
+func (a *cRole) Update(ctx context.Context, req *backend.RoleUpdateReq) (res *backend.RoleUpdateRes, err error) {
+	err = service.Role().Update(ctx, model.RoleUpdateInput{
+		Id: req.Id,
+		RoleCreateUpdateBase: model.RoleCreateUpdateBase{
+			Name: req.Name,
+			Desc: req.Desc,
+		},
+	})
+	return &backend.RoleUpdateRes{Id: req.Id}, nil
+}
+
+func (a *cRole) List(ctx context.Context, req *backend.RoleGetListCommonReq) (res *backend.RoleGetListCommonRes, err error) {
+	getListRes, err := service.Role().GetList(ctx, model.RoleGetListInput{
+		Page: req.Page,
+		Size: req.Size,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &backend.RoleGetListCommonRes{
+		List:  getListRes.List,
+		Page:  getListRes.Page,
+		Size:  getListRes.Size,
+		Total: getListRes.Total}, nil
 }
